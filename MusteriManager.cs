545234439edc16f2cd30f6fbf8dd5dda8ed2bcfc
burçak kasap <burcak_kasap92@hotmail.com>
2021-05-18 12:59:42 +0300

    Add project files.

---
+++ MusteriManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    public class MusteriManager
+    {
+        public void MusteriEkleme(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri başarıyla eklendi: " + musteri.MusteriName + " " + musteri.MusteriSurname);
+        }
+        public void MusteriListele(Musteri musteri)
+        {
+            Console.WriteLine("Müşteriler başarıyla listelendi:" + musteri.MusteriName + " " + musteri.MusteriSurname);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.MusteriName = "aa";
+            musteri1.MusteriSurname = "bb";
+            Musteri musteri2 = new Musteri();
+            musteri2.MusteriName = "cc";
+            musteri2.MusteriSurname = "dd";
+            MusteriManager musterimanager = new MusteriManager();
+            musterimanager.MusteriEkleme(musteri1);
+            musterimanager.MusteriEkleme(musteri2);
+            Musteri[] musteriler = { musteri1, musteri2 };
+            foreach (var musteri in musteriler)
+            {
+                musterimanager.MusteriListele(musteri);
+            }
+
+        }
+    }
+}
